95f8bfffa23de0973127611e0ee72913ac5df775
maadc <markus@schranz.io>
2020-05-27 10:22:46 +0200

    imported the canvas-module interface

--- angular/src/app/modul-verarbeitung/modul-verarbeitung.service.ts
+++ angular/src/app/modul-verarbeitung/modul-verarbeitung.service.ts
@@ -1,19 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {CanvasModule} from "../interfaces/canvas-module";
 declare function io(): any;
 
-interface module {
-  id: string;
-  idHTML: number; //count
-  type: string; //category
-  position: {
-    x: number,
-    y: number,
-    width: number,
-    height: number
-  };
-  content: string;
-}
+
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +13,7 @@ export class ModulVerarbeitungService{
   socket = io();
   constructor(private http: HttpClient) {}
 
-  moduleDelete(object: module){
+  moduleDelete(object: CanvasModule){
     this.socket.emit('delete', (object));
   }
 
