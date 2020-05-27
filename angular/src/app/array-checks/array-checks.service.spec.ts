15425fca65aaf317167499e5b9b98dc3378a523e
maadc <markus@schranz.io>
2020-05-27 11:53:37 +0200

    finished function which tests if object is in array and returns true/false and position

--- angular/src/app/array-checks/array-checks.service.spec.ts
+++ angular/src/app/array-checks/array-checks.service.spec.ts
@@ -17,36 +17,37 @@ describe('ArrayChecksService', () => {
 });
 
 describe('CheckIfEntrieExists in Array', () => {
-  it('should be true', () => {
-    const inputArray: Array<CanvasModule> = [
-      {
-        _id: "123",
-        idHTML: 1, //count
-        type: "text", //category
-        position: {
-          x: 1,
-          y: 1,
-          width: 2,
-          height: 2
-        },
-        content: "test"
+  const inputArray: Array<CanvasModule> = [
+    {
+      _id: "123",
+      idHTML: 0,
+      type: "text",
+      position: {
+        x: 1,
+        y: 1,
+        width: 2,
+        height: 2
       },
-      {
-        _id: "456",
-        idHTML: 2, //count
-        type: "text2", //category
-        position: {
-          x: 1,
-          y: 1,
-          width: 2,
-          height: 2
-        },
-        content: "test2"
-      }
+      content: "test"
+    },
+    {
+      _id: "456",
+      idHTML: 1,
+      type: "text2",
+      position: {
+        x: 1,
+        y: 1,
+        width: 2,
+        height: 2
+      },
+      content: "test2"
+    }
     ]
+
+  it('should be true', () => {
     const checkObject: CanvasModule = {
       _id: "456",
-      idHTML: 2, //count
+      idHTML: 1, //count
       type: "text2", //category
       position: {
         x: 1,
@@ -56,37 +57,10 @@ describe('CheckIfEntrieExists in Array', () => {
       },
       content: "test2"
     }
-
-    expect(ArrayChecksService.checkIfEntriesExists(inputArray, checkObject)).toBeTrue()
+    expect(ArrayChecksService.checkIfEntriesExists(inputArray, checkObject)).toEqual({"exists": true, "position": 1})
   });
 
   it('should be false', () => {
-    const inputArray: Array<CanvasModule> = [
-      {
-        _id: "123",
-        idHTML: 1, //count
-        type: "text", //category
-        position: {
-          x: 1,
-          y: 1,
-          width: 2,
-          height: 2
-        },
-        content: "test"
-      },
-      {
-        _id: "456",
-        idHTML: 2, //count
-        type: "text2", //category
-        position: {
-          x: 1,
-          y: 1,
-          width: 2,
-          height: 2
-        },
-        content: "test2"
-      }
-    ]
     const checkObject: CanvasModule = {
       _id: "465",
       idHTML: 2, //count
@@ -99,8 +73,23 @@ describe('CheckIfEntrieExists in Array', () => {
       },
       content: "test2"
     }
+    expect(ArrayChecksService.checkIfEntriesExists(inputArray, checkObject)).toEqual({"exists": false, "position": undefined})
+  });
 
-    expect(ArrayChecksService.checkIfEntriesExists(inputArray, checkObject)).toBeFalse()
+  it('should return object position 1', () => {
+    const checkObject: CanvasModule = {
+      _id: "123",
+      idHTML: 0,
+      type: "text",
+      position: {
+        x: 1,
+        y: 1,
+        width: 2,
+        height: 2
+      },
+      content: "test"
+    }
+    expect(ArrayChecksService.checkIfEntriesExists(inputArray, checkObject)).toEqual({"exists": true, "position": 0})
   });
 });
 
--- angular/src/app/array-checks/array-checks.service.ts
+++ angular/src/app/array-checks/array-checks.service.ts
@@ -8,13 +8,16 @@ export class ArrayChecksService {
 
   constructor() { }
 
-  static checkIfEntriesExists(array: Array<CanvasModule>, object: object){
-    let exists = false;
-    array.forEach((element) => {
-      if (JSON.stringify(element) === JSON.stringify(object)){
-        exists = true;
+  static checkIfEntriesExists(array: Array<CanvasModule>, object: object):object{
+    let exists:object = {
+      "exists": false,
+      "position": undefined,
+    };
+    for(let i = 0; i < array.length; i++){
+      if (JSON.stringify(array[i]) === JSON.stringify(object)){
+        exists = {"exists": true,"position": i};
       }
-    })
+    }
     return exists;
   }
 }
