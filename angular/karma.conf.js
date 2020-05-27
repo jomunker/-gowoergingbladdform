dc311421399743ccb275c1a14b2ee2a86a4542e1
maadc <markus@schranz.io>
2020-05-27 11:53:02 +0200

    changed settings of spec-reporter

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -24,11 +24,11 @@ module.exports = function (config) {
     reporters: ["spec"],
     specReporter: {
       maxLogLines: 5,             // limit number of lines logged per test
-      suppressErrorSummary: true, // do not print error summary
+      suppressErrorSummary: false, // do not print error summary
       suppressFailed: false,      // do not print information about failed tests
       suppressPassed: false,      // do not print information about passed tests
       suppressSkipped: true,      // do not print information about skipped tests
-      showSpecTiming: false,      // print the time elapsed for each spec
+      showSpecTiming: true,      // print the time elapsed for each spec
       failFast: true              // test would finish with error when a first fail occurs.
     },
     port: 9876,
