b5aa60fe65c654b4df720e9ccd1149d9ff82c1f4
maadc <markus@schranz.io>
2020-05-27 10:22:23 +0200

    moved test to an external service

--- angular/src/app/modul-verarbeitung/modul-verarbeitung.service.spec.ts
+++ angular/src/app/modul-verarbeitung/modul-verarbeitung.service.spec.ts
@@ -15,33 +15,6 @@ describe('ModulVerarbeitungService', () => {
     httpMock = TestBed.inject(HttpTestingController);
   });
 
-  describe('moduleDelete()', () => {
-    it('should throw connection error', () => {
-
-      const object = {
-        "id": "auozhhj21^^12",
-        "idHTML": 1,
-        "type": "text",
-        "position": {"x": 100, "y": 100, "width": 20, "height": 20},
-        "content": "hello world"
-      }
-      const dummy = {"test": 1234}
-
-      // @ts-ignore
-      spy = spyOn(service, "io");
-
-      service.moduleDelete(object).subscribe(response => {
-        expect(response).toEqual(dummy);
-      });
-
-      const req = httpMock.expectOne(`/delete`);
-      expect(req.request.method).toBe("POST");
-      req.flush(dummy);
-      httpMock.verify();
-    })
-  })
-
-
 
 });
 
