0a1469b6fd9aee0af3871aed9bfdfb7b8df09e16
lisaokezie <56400443+lisaokezie@users.noreply.github.com>
2020-06-03 20:31:05 +0200

    split up root into chat, header, canvas, toolbar

--- angular/src/app/components/chat/chat.component.ts
+++ angular/src/app/components/chat/chat.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ArrayChecksService } from '../../services/array-checks/array-checks.service';
+import { CanvasModuleService } from '../../services/canvasmodule/canvasmodule.service';
+import { CanvasModule } from '../../interfaces/canvasModule';
+import { RootComponent } from '../root/root.component';
+
+declare function io(): any;
+
+interface EntryObject {
+  "exists": boolean,
+  "position": number,
+}
 
 @Component({
   selector: 'app-chat',
@@ -6,10 +18,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
+  opened = false;
+  
+  title = 'coworkingplatform';
+  moduleArray = [];
+  socket = io()
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private http: HttpClient, public canvasmoduleservice: CanvasModuleService, public rootComponent: RootComponent) {
   }
+
+    ngOnInit() {
+
+      this.socket.on('chat message', (msg) => {
+        this.moduleArray.push(msg);
+      });
+  
+      this.socket.on('module edited', (moduleEdit) => {
+        // search and replace the edited object in the modules array
+  
+        // @ts-ignore
+        const replaceObject : EntryObject = ArrayChecksService.checkIfEntriesExists(this.moduleArray, moduleEdit)
+  
+        if (replaceObject.exists) {
+          this.moduleArray.splice(replaceObject.position, 1, moduleEdit)
+        } else {
+          console.log(JSON.stringify(moduleEdit) + "can not be edited")
+        }
+      });
+  
+      this.socket.on('delete', (object) => {
+        //find the object to delete, if its exists delete it from the array
+  
+        // @ts-ignore
+        const deleteObject: EntryObject = ArrayChecksService.checkIfEntriesExists(this.moduleArray, object)
+  
+        if (deleteObject.exists){
+          this.moduleArray.splice(deleteObject.position, 1)
+        } else {
+          console.log(JSON.stringify(object) + "can not be deleted")
+        }
+      });
+    }
+  
+  
+    //when this client pushes a message
+    onSend(msg: string) {
+      this.canvasmoduleservice.moduleCreate(msg);
+  
+      //clear the input field
+      const input: any = document.getElementById("input")
+      input.value = "";
+    }
+  
+    loadDB() {
+      const option = {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      }
+  
+      this.http.post('/api/all',option).subscribe(response => {
+        //type change object(which is an array actually) -> any
+        let data: any = response;
+        let newDisplayedArray: Array<String> = [];
+  
+        //go to the whole array and split each item into these two new Arrays
+        for(let i = 0; i < data.length ;i++ ){
+          newDisplayedArray.push(data[i]);
+        }
+        this.moduleArray = newDisplayedArray;
+      });
+    }
 
 }
