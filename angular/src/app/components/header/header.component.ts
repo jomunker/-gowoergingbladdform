71364b6549f55e555e495e2eb61be45d8c223ce3
maadc <markus@schranz.io>
2020-06-05 14:58:02 +0200

    chat gets loaded on opening the side

--- angular/src/app/components/header/header.component.ts
+++ angular/src/app/components/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RootComponent } from '../root/root.component';
+import {ChatService} from "../../services/chat/chat.service";
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import { RootComponent } from '../root/root.component';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(public rootComponent: RootComponent) { }
+  constructor(public rootComponent: RootComponent, public chatService:ChatService) { }
 
   ngOnInit(): void {
   }
