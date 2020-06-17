662e3a11eff677f390a33d228540e32411169d31
lisaokezie <56400443+lisaokezie@users.noreply.github.com>
2020-06-18 00:24:08 +0200

    upload file from app

--- angular/src/app/components/toolbar/toolbar.component.ts
+++ angular/src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CanvasModuleService } from 'src/app/services/canvasmodule/canvasmodule.service';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
@@ -8,9 +10,35 @@ import { CanvasModuleService } from 'src/app/services/canvasmodule/canvasmodule.
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor(public canvasmoduleservice: CanvasModuleService) { }
+  selectedFile: File = null;
+
+  constructor(public canvasmoduleservice: CanvasModuleService, private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+
+  // File will be uploaded as selectio is done
+  onfileChange(element) {
+    console.log(event);
+    this.selectedFile = element.target.files[0];
+    let formData = new FormData();
+        formData.append("image", this.selectedFile, this.selectedFile.name);
+    
+    this.http.post('/upload-image', formData)
+        .subscribe((response) => {
+            console.log('response received is ', response);
+        })
+  }
+
+  // File will be uploaded after user clicks on button
+
+  // upload() {
+  //   let formData = new FormData();
+  //       formData.append("image", this.uploadedFile, this.uploadedFile.name);
+  //   this.http.post('/upload-image', formData)
+  //       .subscribe((response) => {
+  //           console.log('response received is ', response);
+  //       })
+  // }
 
 }
